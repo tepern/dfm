5080c82ba56e6ca412ab1763ee35afd9721fef84
tepern <teper.n@gmail.com>
2018-09-21 18:27:16 +0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+	hot: true,
+	open: true,
+	inline: true	
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+	  {
+        test: /\.scss$/,
+		use: ExtractTextPlugin.extract({
+			fallback: 'style-loader',
+			use: ['css-loader','sass-loader']
+		})
+      },
+	  {
+        test: /\.sass$/,
+		use: ExtractTextPlugin.extract({
+			fallback: 'style-loader',
+			use: ['css-loader','sass-loader']
+		})
+      },
+	  {
+		test: /\.(jpg|png|gif|svg)$/, 
+		loader: 'file-loader',
+		options: {
+			name: 'images/[name].[ext]'
+		},
+	  },
+	  {
+		test: /\.(eot|ttf|woff|woff2)$/, 
+		loader: 'file-loader',
+		options: {
+			name: 'fonts/nobelwgl/[name].[ext]'
+		},
+	  }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+	new ExtractTextPlugin('style.css'),
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
